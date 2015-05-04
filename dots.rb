3e8385f2cd0e5870497f745b0cf169b51a158821
Andreas Tiefenthaler <at@an-ti.eu>
2015-05-04 22:08:37 +0200

    Start from zero
    Because sometimes you need no dots at all.

--- dots.rb
+++ dots.rb
@@ -5,7 +5,7 @@ require 'rake'
 
 task :generate do |task, args|
   how_many = args[:how_many] || 1000
-  (1..how_many).each do |it|
+  (0..how_many).each do |it|
      open("dots/#{it}.dot", "w") do |f|
        f.write('.' * it)
      end
